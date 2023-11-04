308542ad307f6866c8308cbdde7017d2cf049123
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-04 07:31:38 +0000

    [Finish]3-kadai test済]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,27 +2,56 @@ class BooksController < ApplicationController
   def new
   end
 
+
   def index
      @book = Book.new
-
      @books = Book.all
-
   end
+
 
   def create
-
-    book = Book.new(book_params)
-
-    book.save
-    redirect_to 'book_path'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
+
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.all
   end
+
 
   def edit
+     @book = Book.find(params[:id])
+     @books = Book.all
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated"
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :edit
+    end
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed"
+    redirect_to books_path  # 投稿一覧画面へリダイレクト
+  end
+
+private
 def book_params
     params.require(:book).permit(:title, :body)
 end
